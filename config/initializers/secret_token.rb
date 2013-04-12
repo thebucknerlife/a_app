161029e524024bd942a237fdf58b90a4914e3a61
Greg Buckner <greg@theraftapp.com>
2013-04-12 00:21:35 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AriApp::Application.config.secret_token = 'c4ff993d16ad86606153786294e7860dd42ddef7ab6ec19c5ef509da101419109516d1d15c019320a4655d3d61e1a52f1d76633fbcebdd1a51764771c9525934'
